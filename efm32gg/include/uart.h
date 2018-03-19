2bacdcb2a7a44dbfd51e255376f1532913b261a9
eirstore <eirstore@stud.ntnu.no>
2018-03-19 18:09:33 +0100

    Move enum msg_state to vortex_msg.h

--- efm32gg/include/uart.h
+++ efm32gg/include/uart.h
@@ -1,38 +1,17 @@
 #ifndef UART_H_
 #define UART_H_
 
+#include "vortex_msg.h"
+
 #include "em_chip.h"
 #include "em_cmu.h"
 #include "em_gpio.h"
 #include "em_usart.h"
 #include "efm32gg990f1024.h"
 
-enum msg_state
-{
-	MAGIC_BYTES_NOT_RECEIVED,
-	MAGIC_BYTES_RECEIVED,
-	SEND_OK,
-	SEND_FAIL,
-	RECEIVE_OK,
-	RECEIVE_FAIL,
-};
-
-enum msg_type
-{
-	ACK_MSG,
-	NOACK_MSG,
-};
-
 #define BUFFERSIZE 64
-#define MAX_PAYLOAD_SIZE 16
-#define VORTEX_MSG_MAX_SIZE 21
 
 extern struct circularBuffer receiveBuff;
-extern struct vortex_msg message;
-
-#define MAGIC_START_BYTE 	0x69
-#define MAGIC_STOP_BYTE 	0x21
-#define MSG_TYPE_THRUSTER 	0x70
 
 #define UART USART1
 #define UART_PORT gpioPortD
@@ -43,7 +22,7 @@ extern struct vortex_msg message;
 void initUart(void);
 void setupUart(void);
 uint8_t magic_bytes_received(void);
-uint8_t receive_vortex_msg(void);
-uint8_t send_vortex_msg(enum msg_type type);
+uint8_t receive_vortex_msg(uint8_t *receive_data_ptr);
+uint8_t send_vortex_msg(msg_flag type);
 
 #endif //UART_H_
