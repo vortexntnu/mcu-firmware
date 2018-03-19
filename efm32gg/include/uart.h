33c808c5f60fc498850f8c31eb8baece7f95bda5
eirstore <eirstore@stud.ntnu.no>
2018-03-19 10:41:18 +0100

    Add enums msg_state and msg_type
    enum msg_state{} is to be returned by send and receive handlers.
    enum msg_type{} is an argument in send handler.

--- efm32gg/include/uart.h
+++ efm32gg/include/uart.h
@@ -1,16 +1,38 @@
 #ifndef UART_H_
 #define UART_H_
 
-
 #include "em_chip.h"
 #include "em_cmu.h"
 #include "em_gpio.h"
 #include "em_usart.h"
 #include "efm32gg990f1024.h"
 
-#define BUFFERSIZE 20
+enum msg_state
+{
+	MAGIC_BYTES_NOT_RECEIVED,
+	MAGIC_BYTES_RECEIVED,
+	SEND_OK,
+	SEND_FAIL,
+	RECEIVE_OK,
+	RECEIVE_FAIL,
+};
 
-extern volatile struct circularBuffer receiveBuff, transmitBuff;
+enum msg_type
+{
+	ACK_MSG,
+	NOACK_MSG,
+};
+
+#define BUFFERSIZE 64
+#define MAX_PAYLOAD_SIZE 16
+#define VORTEX_MSG_MAX_SIZE 21
+
+extern struct circularBuffer receiveBuff;
+extern struct vortex_msg message;
+
+#define MAGIC_START_BYTE 	0x69
+#define MAGIC_STOP_BYTE 	0x21
+#define MSG_TYPE_THRUSTER 	0x70
 
 #define UART USART1
 #define UART_PORT gpioPortD
@@ -20,7 +42,8 @@ extern volatile struct circularBuffer receiveBuff, transmitBuff;
 
 void initUart(void);
 void setupUart(void);
-void uartGetData(uint8_t* dataPtr);
-void uartPutData(uint8_t* dataPtr);
+uint8_t magic_bytes_received(void);
+uint8_t receive_vortex_msg(void);
+uint8_t send_vortex_msg(enum msg_type type);
 
 #endif //UART_H_
