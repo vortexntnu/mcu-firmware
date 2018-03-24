ce9a832c52586c237d48cfcfe8c4b32a714353c5
eirstore <eirstore@stud.ntnu.no>
2018-03-24 17:25:50 +0100

    Split msg_flag enum into msg_state and msg_type

--- efm32gg/include/vortex_msg.h
+++ efm32gg/include/vortex_msg.h
@@ -11,7 +11,7 @@
 #define VORTEX_MSG_TYPE_INDEX 2
 #define VORTEX_MSG_START_DATA_INDEX 3
 
-typedef enum msg_flag
+typedef enum msg_state
 {
 	MSG_STATE_MAGIC_BYTES_NOT_RECEIVED,
 	MSG_STATE_MAGIC_BYTES_RECEIVED,
@@ -19,12 +19,17 @@ typedef enum msg_flag
 	MSG_STATE_SEND_FAIL,
 	MSG_STATE_RECEIVE_OK,
 	MSG_STATE_RECEIVE_FAIL,
+}msg_state;
+
+typedef enum msg_type
+{
 	MSG_TYPE_NOTYPE,
-	MSG_TYPE_THRUSTER = 0x41,
-	MSG_TYPE_LED,
 	MSG_TYPE_ACK,
 	MSG_TYPE_NOACK,
-}msg_flag;
+	MSG_TYPE_THRUSTER = 0x41,
+	MSG_TYPE_LED = 0x42,
+	MSG_TYPE_HEARTBEAT = 0x43,
+}msg_type;
 
 
 #endif
