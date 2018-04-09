2d3fa66145d70064887cb97043f4e41435d26e13
eirstore <eirstore@stud.ntnu.no>
2018-04-09 14:30:35 +0200

    Correctly include efm32gg942f1024.h
    Starter Kit MCU is named efm32gg990f1024.h and the MCU on the PCB
    is named efm32gg942f1024.h.
    
    Move several rov related function to "rov_utilities".

--- efm32gg/src/main.c
+++ efm32gg/src/main.c
@@ -2,29 +2,25 @@
 
 #include "em_rmu.h"
 #include "em_letimer.h"
-#include "efm32gg990f1024.h"
+#include "efm32gg942f1024.h"
 
 #include "uart.h"
 #include "pwm.h"
-#include "vortex_msg.h"
 #include "crc.h"
 #include "watchdog.h"
-
-#define LETIMER_MS 2000 // how often LETIMER0_IRQHandler triggers in milliseconds
-
-bool start_sequence_finished = false;
-bool is_start_sequence_finished();
-
-bool crc_passed(uint8_t * receive_data);
-void start_sequence(void);
-void initleTimer(void);
+#include "rov_utilities.h"
 
 int main()
 {
 	CHIP_Init();
+
 	initUart();
 	initPwm();
 	initWdog();
+	initLeTimer();
+
+	GPIO_PinModeSet(LED1_PORT, LED1_PIN, gpioModePushPullDrive, 1);
+	GPIO_PinModeSet(LED2_PORT, LED2_PIN, gpioModePushPullDrive, 1);
 
 	unsigned long resetCause = RMU_ResetCauseGet();
 	RMU_ResetCauseClear();
@@ -34,12 +30,13 @@ int main()
 
 	if (resetCause & RMU_RSTCAUSE_WDOGRST)
 	{
-		strcpy(startup_msg_ptr, "MCU reset by watchdog, start initialization...\n\r");
+		strcpy(startup_msg_ptr, "Please pet the watchdog, start initialization...\n\r");
 	}
 	else
 	{
-		strcpy(startup_msg_ptr, "$ MCU reset normally, start initialization... @\n\r");
+		strcpy(startup_msg_ptr, "MCU reset normally, start initialization...\n\r");
 	}
+
 
 	USART_PutData((uint8_t*)startup_msg_ptr, strlen(startup_msg));
 
@@ -49,8 +46,7 @@ int main()
 
 	start_sequence();
 
-
-	strcpy(&startup_msg[0], "$ MCU initialization finished... @\n\r");
+	strcpy(&startup_msg[0], "MCU initialization finished...\n\r");
 	USART_PutData((uint8_t*)startup_msg_ptr, strlen(startup_msg));
 
 	while (1)
@@ -68,8 +64,6 @@ int main()
 					send_vortex_msg(MSG_TYPE_NOACK);
 					msg_type = MSG_TYPE_NOACK;
 				}
-
-				WDOGn_Feed(WDOG);
 				break;
 
 			case MSG_STATE_RECEIVE_FAIL:
@@ -81,7 +75,7 @@ int main()
 				break;
 		}
 
-		switch(msg_type)
+		switch (msg_type)
 		{
 			case MSG_TYPE_NOTYPE:
 				// check how long since last heartbeat?
@@ -101,6 +95,14 @@ int main()
 				}
 				break;
 
+			case MSG_TYPE_ARM:
+				arm_sequence();
+				break;
+
+			case MSG_TYPE_DISARM:
+				disarm_sequence();
+				break;
+
 			case MSG_TYPE_HEARTBEAT:
 				WDOGn_Feed(WDOG);
 				break;
@@ -110,93 +112,3 @@ int main()
 		}
 	}
 }
-
-bool crc_passed(uint8_t * receive_data)
-{
-	uint16_t crc_checksum_calc = 0;
-	uint16_t crc_checksum_received = 1;
-
-	crc_checksum_calc = crc_16(&receive_data[VORTEX_MSG_START_DATA_INDEX], MAX_PAYLOAD_SIZE);
-	crc_checksum_received = (uint16_t)((receive_data[VORTEX_MSG_CRC_BYTE_INDEX] << 8) & 0xFF00)
-							| (uint16_t)((receive_data[VORTEX_MSG_CRC_BYTE_INDEX+1]) & 0x00FF);
-
-	if (crc_checksum_calc == crc_checksum_received)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-
-void start_sequence(void)
-{
-	initleTimer();
-
-	// startup sequence for leds, thrusters are already being initialized
-
-	GPIO_PinModeSet(gpioPortE, 3, gpioModePushPull, 0);
-	GPIO_PinOutSet(gpioPortE, 3);
-
-	while (!is_start_sequence_finished())
-	{
-		// wait for LETIMER_MS milliseconds
-		WDOGn_Feed(WDOG);
-	}
-}
-
-bool is_start_sequence_finished()
-{
-	return start_sequence_finished;
-}
-
-
-void LETIMER0_IRQHandler(void)
-{
-	// Clear LETIMER0 underflow interrupt flag
-	start_sequence_finished = true;
-	LETIMER_IntClear(LETIMER0, LETIMER_IF_UF);
-	LETIMER0->CNT = LETIMER_MS;
-	NVIC_DisableIRQ(LETIMER0_IRQn);
-}
-
-
-void initleTimer(void)
-{
-	const LETIMER_Init_TypeDef letimerInit =
-	{
-	  .enable         = false,             // Start counting when init completed */
-	  .debugRun       = true,            // Counter shall not keep running during debug halt */
-	  .comp0Top       = true,             // Load COMP0 register into CNT when counter underflows. COMP0 is used as TOP */
-	  .bufTop         = false,            // Don't load COMP1 into COMP0 when REP0 reaches 0 */
-	  .out0Pol        = 0,                // Idle value for output 0 */
-	  .out1Pol        = 0,                // Idle value for output 1 */
-	  .ufoa0          = letimerUFOAToggle,// Toggle output on output 0 */
-	  .ufoa1          = letimerUFOAToggle,// Toggle output on output 1 */
-	  .repMode        = letimerRepeatFree // Count until stopped */
-	};
-
-	CMU_ClockEnable(cmuClock_CORELE, true);
-
-	CMU_OscillatorEnable(cmuOsc_ULFRCO, true, true);
-
-	// The clock source of LETIMER0
-	CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_ULFRCO);
-
-	CMU_ClockEnable(cmuClock_LETIMER0, true);
-
-	// Time out value configuration
-	LETIMER_CompareSet(LETIMER0, 0, LETIMER_MS);
-
-	// Initializing LETIMER0
-	LETIMER_Init(LETIMER0, &letimerInit);
-	LETIMER_Enable(LETIMER0, true);
-
-	LETIMER_IntClear(LETIMER0, _LETIMER_IF_MASK);
-	LETIMER_IntEnable(LETIMER0, LETIMER_IF_UF);
-	NVIC_ClearPendingIRQ(LETIMER0_IRQn);
-	NVIC_EnableIRQ(LETIMER0_IRQn);
-
-}
-
