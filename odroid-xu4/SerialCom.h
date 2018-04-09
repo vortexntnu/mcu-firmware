c1c6f7a6436ff124395620266b1cce0b40b8ca5c
eirstore <eirstore@stud.ntnu.no>
2018-04-09 14:43:41 +0200

    Add vortex_msg defines and function headers

--- odroid-xu4/SerialCom.h
+++ odroid-xu4/SerialCom.h
@@ -3,9 +3,10 @@
 
 #include "ros/ros.h"
 #include "vortex_msgs/Pwm.h"
+#include "std_msgs/String.h"
 
 #include <stdio.h>
-#include <stdlib.h>     //
+#include <stdlib.h> 
 #include <string.h>     // string function definitions
 #include <unistd.h>     // UNIX standard function definitions
 #include <fcntl.h>      // File control definitions
@@ -17,27 +18,36 @@ extern "C" {
   #include "crc.h"
 }
 
+#define MAGIC_START_BYTE    0x24
+#define MSG_TYPE_THRUSTER   0x41
+#define MSG_TYPE_LIGHT      0x42
+#define MSG_TYPE_HEARTBEAT  0x43
+#define MSG_TYPE_ARM        0x46
+#define MSG_TYPE_DISARM     0x47
+#define MAGIC_STOP_BYTE     0x40
+
 #define MAX_PAYLOAD_SIZE 16
 #define MAX_MSG_SIZE 21
+#define MSG_HEARTBEAT_SIZE 3
 
-#define MAGIC_START_BYTE_INDEX 0
-#define MAGIC_STOP_BYTE_INDEX 20 // 18 without crc bytes, 20 with crc bytes
-
-#define MSG_CRC_BYTE_INDEX 18
-
+#define MAGIC_START_BYTE_INDEX  0
+#define MSG_TYPE_INDEX          1
 #define MSG_PAYLOAD_START_INDEX 2
-#define MSG_PAYLOAD_STOP_INDEX 17
-
-#define MSG_TYPE_INDEX 1
+#define MSG_PAYLOAD_STOP_INDEX  17
+#define MSG_CRC_BYTE_INDEX      18
+#define MAGIC_STOP_BYTE_INDEX   20
 
 class SerialCom
 {
         private:
-                int m_dev;
+                int m_dev = 0;
                 char m_read_buffer[MAX_MSG_SIZE];
-                char m_cmd[MAX_MSG_SIZE] = {0};
+                char m_thruster_cmd[MAX_MSG_SIZE];
+                char m_heartbeat_cmd[MSG_HEARTBEAT_SIZE];
+
         public:
-                void callback(const vortex_msgs::Pwm& msg);
+                void thruster_pwm_callback(const vortex_msgs::Pwm& msg);
+                void heartbeat_callback(const std_msgs::String::ConstPtr& msg);
                 int serial_write(char* cmd, int cmd_size);
                 uint16_t crc_checksum(char* input, uint8_t num);
                 int serial_read();
